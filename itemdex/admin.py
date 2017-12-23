43a01d8e44ce90d4c4428c8494569485cbf3c10c
Pdom <ahb0327@naver.com>
2017-12-24 02:29:58 +0900

    Add Item model

--- itemdex/admin.py
+++ itemdex/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from itemdex.models import Item
 
-# Register your models here.
+admin.site.register(Item)
--- itemdex/models.py
+++ itemdex/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+	game_id		= models.IntegerField(default = 0)
+	category	= models.IntegerField(default = 0)
+	name		= models.CharField(max_length = 20)
+	text		= models.CharField(max_length = 500)
+
+	def __str__(self):
+		return self.name
