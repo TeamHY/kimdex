fc11a71cfd29c554964a88fc41f4c5cb5ab70f29
Pdom <ahb0327@naver.com>
2017-12-25 21:34:32 +0900

    Update name field max_length

--- itemdex/models.py
+++ itemdex/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Item(models.Model):
 	game_id		= models.IntegerField(default = 0)
 	category	= models.IntegerField(default = 0)
-	name		= models.CharField(max_length = 20)
+	name		= models.CharField(max_length = 50)
 	text		= models.CharField(max_length = 500)
 
 	def __str__(self):
